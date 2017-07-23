a7ba5b601faa1f2b443e38a24a4c0a2a32fe935b
sergmain <serg_main@yahoo.com>
2017-07-23 23:45:09 +0300

    definition of dataset from csv file

--- src/main/java/aiai/ai/launchpad/dataset/DatasetsController.java
+++ src/main/java/aiai/ai/launchpad/dataset/DatasetsController.java
@@ -12,7 +12,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,7 +55,7 @@ public class DatasetsController {
     }
 
     @GetMapping("/datasets")
-    public String init(@ModelAttribute Result result, @PageableDefault(size=5) Pageable pageable)  {
+    public String init(@ModelAttribute Result result, @PageableDefault(size = 5) Pageable pageable) {
         pageable = fixPageSize(pageable);
         result.items = repository.findAll(pageable);
         return "/launchpad/datasets";
@@ -61,7 +63,7 @@ public class DatasetsController {
 
     // for AJAX
     @PostMapping("/datasets-part")
-    public String getDatasets(@ModelAttribute Result result, @PageableDefault(size=5) Pageable pageable )  {
+    public String getDatasets(@ModelAttribute Result result, @PageableDefault(size = 5) Pageable pageable) {
         pageable = fixPageSize(pageable);
         result.items = repository.findAll(pageable);
         return "/launchpad/datasets :: table";
@@ -74,13 +76,13 @@ public class DatasetsController {
     }
 
     @GetMapping(value = "/dataset-edit/{id}")
-    public String edit(@PathVariable Long id, Model model){
+    public String edit(@PathVariable Long id, Model model) {
         model.addAttribute("dataset", repository.findById(id));
         return "/launchpad/dataset-form";
     }
 
     @GetMapping(value = "/dataset-definition/{id}")
-    public String toDatasetDefinition(@PathVariable(name="id") Long datasetId, Model model){
+    public String toDatasetDefinition(@PathVariable(name = "id") Long datasetId, Model model) {
         final Optional<Dataset> datasetOptional = repository.findById(datasetId);
         if (!datasetOptional.isPresent()) {
             return "redirect:/launchpad/datasets";
@@ -88,27 +90,27 @@ public class DatasetsController {
 
         final DatasetDefinition definition = new DatasetDefinition(datasetOptional.get());
         final Dataset dataset = definition.dataset;
-        
+
         final int groupSize = dataset.getDatasetGroups().size();
         for (int i = 0; i < groupSize; i++) {
             DatasetGroup group = dataset.getDatasetGroups().get(i);
             group.setAddColumn(false);
-            if (i>0) {
-                DatasetGroup groupPrev = dataset.getDatasetGroups().get(i-1);
+            if (i > 0) {
+                DatasetGroup groupPrev = dataset.getDatasetGroups().get(i - 1);
                 if (groupPrev.getDatasetColumns().isEmpty()) {
                     break;
                 }
             }
 
-            if (groupSize==1) {
+            if (groupSize == 1) {
                 group.setAddColumn(true);
                 continue;
             }
-            if (i==groupSize-1) {
+            if (i == groupSize - 1) {
                 group.setAddColumn(true);
                 continue;
             }
-            DatasetGroup groupNext = dataset.getDatasetGroups().get(i+1);
+            DatasetGroup groupNext = dataset.getDatasetGroups().get(i + 1);
             if (groupNext.getDatasetColumns().isEmpty()) {
                 group.setAddColumn(true);
                 continue;
@@ -118,18 +120,18 @@ public class DatasetsController {
         // last actual column in groups. there isn't any non-empty group after this one
         for (int i = 0; i < dataset.getDatasetGroups().size(); i++) {
             // case when last group isn't empty
-            if (i+1==dataset.getDatasetGroups().size()) {
+            if (i + 1 == dataset.getDatasetGroups().size()) {
                 final List<DatasetColumn> columns = dataset.getDatasetGroups().get(i).getDatasetColumns();
-                columns.get(columns.size()-1).setLastColumn(true);
+                columns.get(columns.size() - 1).setLastColumn(true);
                 break;
             }
             // case when there are some empty groups
-            if (i<dataset.getDatasetGroups().size()-1 && dataset.getDatasetGroups().get(i+1).getDatasetColumns().size()==0) {
+            if (i < dataset.getDatasetGroups().size() - 1 && dataset.getDatasetGroups().get(i + 1).getDatasetColumns().size() == 0) {
                 final List<DatasetColumn> columns = dataset.getDatasetGroups().get(i).getDatasetColumns();
                 if (columns.isEmpty()) {
-                    continue; 
+                    continue;
                 }
-                columns.get(columns.size()-1).setLastColumn(true);
+                columns.get(columns.size() - 1).setLastColumn(true);
                 break;
             }
         }
@@ -141,13 +143,13 @@ public class DatasetsController {
     @GetMapping(value = "/dataset-column-add/{id}")
     public String addColumn(@PathVariable(name = "id") Long datasetGroupId, Model model) {
         final DatasetColumn column = new DatasetColumn();
-        column.setDatasetGroup( groupsRepository.findById(datasetGroupId).get() );
+        column.setDatasetGroup(groupsRepository.findById(datasetGroupId).get());
         model.addAttribute("column", column);
         return "/launchpad/dataset-column-form";
     }
 
     @PostMapping(value = "/dataset-column-add-commit")
-    public String addNewColumnCommit(DatasetColumn column){
+    public String addNewColumnCommit(DatasetColumn column) {
         final Optional<DatasetGroup> value = groupsRepository.findById(column.getDatasetGroup().getId());
         if (!value.isPresent()) {
             return "redirect:/launchpad/datasets";
@@ -156,13 +158,13 @@ public class DatasetsController {
 
         column.setDatasetGroup(group);
         column.setDataset(group.getDataset());
-        columnRepository.save( column );
+        columnRepository.save(column);
 
-        return "redirect:/launchpad/dataset-definition/"+group.getDataset().getId();
+        return "redirect:/launchpad/dataset-definition/" + group.getDataset().getId();
     }
 
     @GetMapping(value = "/dataset-column-move-prev-group/{id}")
-    public String moveColumnToPrevGroup(@PathVariable Long id){
+    public String moveColumnToPrevGroup(@PathVariable Long id) {
         final Optional<DatasetColumn> value = columnRepository.findById(id);
         if (!value.isPresent()) {
             return "redirect:/launchpad/datasets";
@@ -171,14 +173,14 @@ public class DatasetsController {
 
         final long datasetId = column.getDataset().getId();
         List<DatasetGroup> groups = groupsRepository.findByDataset_Id(datasetId);
-        if (groups.size() <2) {
+        if (groups.size() < 2) {
             return "redirect:/launchpad/datasets";
         }
 
         DatasetGroup prevGroup = null;
         for (DatasetGroup group : groups) {
-            if (column.getDatasetGroup().getId()==group.getId()) {
-                if (prevGroup==null) {
+            if (column.getDatasetGroup().getId() == group.getId()) {
+                if (prevGroup == null) {
                     return "redirect:/launchpad/datasets";
                 }
                 column.setDatasetGroup(prevGroup);
@@ -187,12 +189,12 @@ public class DatasetsController {
             prevGroup = group;
         }
 
-        columnRepository.save( column );
-        return "redirect:/launchpad/dataset-definition/"+datasetId;
+        columnRepository.save(column);
+        return "redirect:/launchpad/dataset-definition/" + datasetId;
     }
 
     @GetMapping(value = "/dataset-column-move-next-group/{id}")
-    public String moveColumnToNextGroup(@PathVariable Long id){
+    public String moveColumnToNextGroup(@PathVariable Long id) {
         final Optional<DatasetColumn> value = columnRepository.findById(id);
         if (!value.isPresent()) {
             return "redirect:/launchpad/datasets";
@@ -201,41 +203,41 @@ public class DatasetsController {
 
         final long datasetId = column.getDataset().getId();
         List<DatasetGroup> groups = groupsRepository.findByDataset_Id(datasetId);
-        if (groups.size() <2) {
+        if (groups.size() < 2) {
             return "redirect:/launchpad/datasets";
         }
 
-        for (int i = 0; i < groups.size()-1; i++) {
+        for (int i = 0; i < groups.size() - 1; i++) {
             DatasetGroup group = groups.get(i);
-            if (column.getDatasetGroup().getId()==group.getId()) {
-                DatasetGroup nextGroup = groups.get(i+1);
+            if (column.getDatasetGroup().getId() == group.getId()) {
+                DatasetGroup nextGroup = groups.get(i + 1);
                 column.setDatasetGroup(nextGroup);
                 break;
             }
         }
 
-        columnRepository.save( column );
-        return "redirect:/launchpad/dataset-definition/"+datasetId;
+        columnRepository.save(column);
+        return "redirect:/launchpad/dataset-definition/" + datasetId;
     }
 
     @GetMapping(value = "/dataset-delete-group/{id}")
-    public String deleteGroup(@PathVariable Long id){
+    public String deleteGroup(@PathVariable Long id) {
         final Optional<DatasetGroup> value = groupsRepository.findById(id);
         if (!value.isPresent()) {
             return "redirect:/launchpad/datasets";
         }
         DatasetGroup group = value.get();
         long datasetId = group.getDataset().getId();
-        if ( !group.getDatasetColumns().isEmpty() ) {
+        if (!group.getDatasetColumns().isEmpty()) {
             return "redirect:/launchpad/datasets";
         }
 
-        groupsRepository.delete( group );
-        return "redirect:/launchpad/dataset-definition/"+datasetId;
+        groupsRepository.delete(group);
+        return "redirect:/launchpad/dataset-definition/" + datasetId;
     }
 
     @GetMapping(value = "/dataset-group-add-new/{id}")
-    public String addNewGroup(@PathVariable(name = "id") Long datasetId){
+    public String addNewGroup(@PathVariable(name = "id") Long datasetId) {
         final Optional<Dataset> value = repository.findById(datasetId);
         if (!value.isPresent()) {
             return "redirect:/launchpad/datasets";
@@ -256,31 +258,46 @@ public class DatasetsController {
 
         dataset.getDatasetGroups().add(group);
 
-        repository.save( dataset );
-        return "redirect:/launchpad/dataset-definition/"+datasetId;
+        repository.save(dataset);
+        return "redirect:/launchpad/dataset-definition/" + datasetId;
     }
 
     @PostMapping(value = "/dataset-group-skip-commit")
-    public String setSkipForGroup(Long id, boolean skip){
+    public String setSkipForGroup(Long id, boolean skip) {
         final Optional<DatasetGroup> value = groupsRepository.findById(id);
         if (!value.isPresent()) {
             return "redirect:/launchpad/datasets";
         }
         DatasetGroup group = value.get();
         group.setSkip(skip);
-        groupsRepository.save( group );
+        groupsRepository.save(group);
 
-        return "redirect:/launchpad/dataset-definition/"+group.getDataset().getId();
+        return "redirect:/launchpad/dataset-definition/" + group.getDataset().getId();
+    }
+
+
+    @PostMapping(value = "/dataset-group-from-file")
+    public String createDefinitionFromFile(MultipartFile file, long id, boolean skip ) {
+
+        String line = null;
+        try (InputStream is = file.getInputStream(); final InputStreamReader isr = new InputStreamReader(is, "UTF-8"); BufferedReader br = new BufferedReader(isr)) {
+            line = br.readLine();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return "redirect:/launchpad/dataset-definition/" + id;
     }
 
     @PostMapping("/dataset-form-commit")
     public String formCommit(Dataset dataset) {
-        repository.save( dataset );
+        repository.save(dataset);
         return "redirect:/launchpad/datasets";
     }
 
     @GetMapping("/dataset-delete/{id}")
-    public String delete(@PathVariable Long id, Model model){
+    public String delete(@PathVariable Long id, Model model) {
         final Optional<Dataset> value = repository.findById(id);
         if (!value.isPresent()) {
             return "redirect:/launchpad/datasets";
@@ -296,7 +313,7 @@ public class DatasetsController {
     }
 
     private Pageable fixPageSize(Pageable pageable) {
-        if (pageable.getPageSize()!=limit) {
+        if (pageable.getPageSize() != limit) {
             pageable = PageRequest.of(pageable.getPageNumber(), limit);
         }
         return pageable;
