a98e8d1064d5798abd882bfa85771b63d12c0b69
sergmain <serg_main@yahoo.com>
2018-10-24 12:07:14 -0700

    .

--- mvn-all.bat
+++ mvn-all.bat
@@ -1,2 +1,2 @@
-mvn.cmd clean install -f pom.xml -Dmaven.test.skip=true
-mvn.cmd clean package -Dmaven.test.skip=true
+mvnw.cmd clean install -f pom.xml -Dmaven.test.skip=true
+mvnw.cmd clean package -Dmaven.test.skip=true
