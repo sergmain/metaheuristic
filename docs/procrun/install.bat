7d442aa0884c9cfd3bc8671b8d3dc485f88e262b
sergmain <serg_main@yahoo.com>
2018-10-30 19:55:40 -0700

    .

--- docs/procrun/install.bat
+++ docs/procrun/install.bat
@@ -1,5 +1,5 @@
 prunsrv.exe //IS//Procrun-Aiai --DisplayName="Procrun-Aiai" --Description="Procrun-Aiai" ^
-							--Startup=auto --Install=%CD%\prunsrv.exe --Jvm=auto --Classpath=%CD%\..\git\apps\aiai\target\aiai.jar ^
+							--Startup=auto --Install=%CD%\prunsrv.exe --Jvm=auto --Classpath=%CD%\git\apps\aiai\target\aiai.jar ^
                             --StartMode=jvm --StartClass=aiai.ai.Bootstrap --StartMethod=start --StartParams=start ^
 							--StopMode=jvm --StopClass=aiai.ai.Bootstrap --StopMethod=stop --StopParams=stop ^
 							--StdOutput=auto --StdError=auto --LogPath=%CD% --LogLevel=Debug ^
