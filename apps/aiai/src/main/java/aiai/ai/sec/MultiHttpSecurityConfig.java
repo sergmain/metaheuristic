456c2a52f23f003539d61eb9d8f25a797d004131
sergmain <serg_main@yahoo.com>
2019-03-06 20:15:48 -0800

    .

--- apps/aiai/src/main/java/aiai/ai/sec/MultiHttpSecurityConfig.java
+++ apps/aiai/src/main/java/aiai/ai/sec/MultiHttpSecurityConfig.java
@@ -30,6 +30,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * User: Serg
@@ -123,6 +128,8 @@ public class MultiHttpSecurityConfig {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
+                    .cors()
+                    .and()
 //                    .csrf().csrfTokenRepository(csrfTokenRepository)
 //                    .and()
                     .csrf().disable()
@@ -132,6 +139,18 @@ public class MultiHttpSecurityConfig {
                     .antMatcher("/ng/**").authorizeRequests().anyRequest().authenticated()
                     .and()
                     .httpBasic();
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Arrays.asList("*"));
+            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+            configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+            configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
         }
     }
 
