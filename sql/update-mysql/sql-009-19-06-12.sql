7bae16874bb3d9b09973f9b3b578a77ee7b45994
sergmain <serg_main@yahoo.com>
2019-06-12 17:11:09 -0700

    .

---
+++ sql/update-mysql/sql-009-19-06-12.sql
@@ -0,0 +1,3 @@
+rename table pilot_batch to mh_batch;
+
+rename table pilot_batch_workbook to mh_batch_workbook;
---
+++ sql/update-postgresql/sql-009-19-06-12.sql
@@ -0,0 +1,4 @@
+alter table pilot_batch rename to mh_batch;
+
+
+alter table pilot_batch_workbook rename to mh_batch_workbook;
