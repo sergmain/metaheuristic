8786b1d73ab0353f92fb96939aadb00f067e3aff
sergmain <serg_main@yahoo.com>
2019-06-14 18:20:58 -0700

    .

--- apps/angular/src/environments/environment.ts
+++ apps/angular/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:8081'
+    baseUrl: 'http://localhost:8080'
+    // baseUrl: 'http://localhost:8081'
     // baseUrl: "https://cb208394.ap.ngrok.io"
 };
 
