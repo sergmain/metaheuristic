6b07cad7a4f89b8827cd4f688ca15babfd4935ed
sergmain <serg_main@yahoo.com>
2019-07-26 16:08:09 -0700

    .

--- apps/metaheuristic/src/main/java/ai/metaheuristic/ai/Schedulers.java
+++ apps/metaheuristic/src/main/java/ai/metaheuristic/ai/Schedulers.java
@@ -201,7 +201,7 @@ public class Schedulers {
         /**
          * this scheduler is being run at the station side
          */
-        @Scheduled(initialDelay = 5_000, fixedDelayString = "#{ T(ai.metaheuristic.ai.utils.EnvProperty).minMax( environment.getProperty('ai.metaheuristic.station.timeout.request-launchpad'), 3, 20, 3)*1000 }")
+        @Scheduled(initialDelay = 5_000, fixedDelayString = "#{ T(ai.metaheuristic.ai.utils.EnvProperty).minMax( environment.getProperty('ai.metaheuristic.station.timeout.request-launchpad'), 3, 20, 6)*1000 }")
         public void launchRequester() {
             if (globals.isUnitTesting) {
                 return;
