196cc05dc1944cda515b0570a61d15cc65580138
sergmain <serg_main@yahoo.com>
2019-10-07 16:54:46 -0700

    .

--- apps/metaheuristic/src/main/java/ai/metaheuristic/ai/launchpad/server/ServerService.java
+++ apps/metaheuristic/src/main/java/ai/metaheuristic/ai/launchpad/server/ServerService.java
@@ -278,7 +278,6 @@ public class ServerService {
             lcpy.reAssignedStationId = reassignStationId(remoteAddress, "Id was reassigned from " + stationId);
             return;
         }
-
         StationStatus ss;
         try {
             ss = StationStatusUtils.to(station.status);
