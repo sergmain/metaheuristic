49162ad91fb073001308680feda1bcb4fc6a7e37
sergmain <serg_main@yahoo.com>
2020-03-13 00:00:46 -0700

    .

--- sql/update-mysql/sql-004-20-03-10.sql
+++ sql/update-mysql/sql-004-20-03-10.sql
@@ -12,3 +12,13 @@ set params = replace(PARAMS, 'metrics: true', '');
 
 update mh_function
 set params = replace(PARAMS, 'version: 2', 'version: 1');
+
+update mh_processor
+set status = replace(status, 'snippetCode: ', 'functionCode: ');
+
+update mh_processor
+set status = replace(status, 'snippetState: ', 'functionState: ');
+
+update mh_processor
+set status = replace(status, ': snippet_config_error', ': function_config_error');
+
