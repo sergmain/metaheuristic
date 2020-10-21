63f6a94f7be2d52c8c53c332325a8f9313376c5e
sergmain <serg_main@yahoo.com>
2020-10-21 11:36:31 -0700

    .

--- apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/function/FunctionTopLevelService.java
+++ apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/function/FunctionTopLevelService.java
@@ -313,8 +313,7 @@ public class FunctionTopLevelService {
                         }
                     }
                     else {
-                        status = new FunctionApiData.FunctionConfigStatus(false,"#295.230 Fatal error - file is null ");
-//                        functionService.createFunctionWithData(scy, null, 0);
+                        functionService.createFunctionWithData(scy, null, 0);
                     }
                 }
             }
