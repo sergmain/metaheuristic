4a910e460d27ed439f83a5bc9e816954e2915ce3
sergmain <serg_main@yahoo.com>
2021-05-04 23:31:58 -0700

    .

--- apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/beans/Heuristic.java
+++ apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/beans/Heuristic.java
@@ -16,10 +16,83 @@
 
 package ai.metaheuristic.ai.dispatcher.beans;
 
+import ai.metaheuristic.ai.yaml.exec_context.ExecContextParamsYamlUtils;
+import ai.metaheuristic.api.data.exec_context.ExecContextParamsYaml;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
 /**
  * @author Serge
  * Date: 5/3/2021
  * Time: 11:47 PM
  */
-public class Heuristic {
+@Entity
+@Table(name = "MH_HEURISTIC")
+@Data
+@EqualsAndHashCode(of = {"id", "version"})
+@ToString(exclude={"ecpy"})
+@NoArgsConstructor
+public class Heuristic implements Serializable {
+    private static final long serialVersionUID = -2054668981438811654L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Version
+    public Integer version;
+
+    // This field contains a value from MH_COMPANY.UNIQUE_ID, !NOT! from ID field
+    @NotNull
+    @Column(name = "COMPANY_ID")
+    public Long companyId;
+
+    @Column(name="CREATED_ON")
+    public long createdOn;
+
+    @NotBlank
+    @Column(name = "PARAMS")
+    private String params;
+
+    public void setParams(String params) {
+        synchronized (this) {
+            this.params = params;
+            this.ecpy = null;
+        }
+    }
+
+    @Column(name = "IS_DELETED")
+    public boolean deleted;
+
+    @Transient
+    @JsonIgnore
+    @Nullable
+    private ExecContextParamsYaml ecpy = null;
+
+    @JsonIgnore
+    public ExecContextParamsYaml getExecContextParamsYaml() {
+        if (ecpy ==null) {
+            synchronized (this) {
+                if (ecpy ==null) {
+                    ExecContextParamsYaml temp = ExecContextParamsYamlUtils.BASE_YAML_UTILS.to(params);
+                    ecpy = temp==null ? new ExecContextParamsYaml() : temp;
+                }
+            }
+        }
+        return ecpy;
+    }
+
+    @JsonIgnore
+    public void updateParams(ExecContextParamsYaml wpy) {
+        setParams(ExecContextParamsYamlUtils.BASE_YAML_UTILS.toString(wpy));
+    }
 }
