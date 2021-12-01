8b50b22a03660fbb98cfbe28a8b5a543f217d6cf
sergmain <serg_main@yahoo.com>
2021-11-30 22:16:02 -0800

    .

--- apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/task/TaskQueue.java
+++ apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/task/TaskQueue.java
@@ -373,7 +373,7 @@ public class TaskQueue {
                 // state from CHECK_CASHE to NONE is being changing without assigning
                 else if (!task.assigned && state!=EnumsApi.TaskExecState.NONE) {
                     log.warn("#029.260 State of task #{} {} can't be changed to {} because the task wasn't assigned.",
-                            task.queuedTask.task==null ? null : EnumsApi.TaskExecState.from(task.queuedTask.task.execState), task.queuedTask.taskId, state);
+                            task.queuedTask.task==null ? null : "<null>", task.queuedTask.taskId, state);
                     try {
                         throw new RuntimeException("This isn't actual an error, only for stacktrace:");
                     }
