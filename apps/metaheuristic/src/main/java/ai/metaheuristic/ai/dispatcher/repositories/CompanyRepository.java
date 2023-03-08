4ed528e8134e65fd62066320fc344b24b0c2b4f3
sergmain <serg_main@yahoo.com>
2023-03-08 10:25:42 -0800

    .

--- apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/repositories/CompanyRepository.java
+++ apps/metaheuristic/src/main/java/ai/metaheuristic/ai/dispatcher/repositories/CompanyRepository.java
@@ -26,7 +26,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
