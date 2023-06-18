f50a92a7ab5500ffb1b7ca119539b53c555fc5ad
sergmain <serg_main@yahoo.com>
2023-06-17 19:39:29 -0700

    .

--- apps/metaheuristic/src/test/java/ai/metaheuristic/mhbp/openai/QueryOpenaiTest.java
+++ apps/metaheuristic/src/test/java/ai/metaheuristic/mhbp/openai/QueryOpenaiTest.java
@@ -18,6 +18,7 @@
 package ai.metaheuristic.mhbp.openai;
 
 import ai.metaheuristic.ai.utils.RestUtils;
+import lombok.AllArgsConstructor;
 import org.apache.commons.io.IOUtils;
 import org.apache.hc.client5.http.fluent.Content;
 import org.apache.hc.client5.http.fluent.Executor;
@@ -31,6 +32,7 @@ import org.apache.hc.core5.net.URIBuilder;
 import org.apache.hc.core5.util.Timeout;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.lang.Nullable;
 
 import java.io.IOException;
 import java.net.URI;
@@ -45,7 +47,18 @@ import java.nio.charset.StandardCharsets;
 @Disabled
 public class QueryOpenaiTest {
 
-    String json = """
+    @AllArgsConstructor
+    public static class Cfg {
+        @Nullable
+        String url;
+        String prompt;
+
+        public Cfg(String prompt) {
+            this.prompt = prompt;
+        }
+    }
+
+    Cfg json = new Cfg("""
             {
               "model": "text-davinci-003",
               "prompt": "answer 2+2 with only digit",
@@ -56,44 +69,66 @@ public class QueryOpenaiTest {
               "presence_penalty": 0.6,
               "stop": [" Human:", " AI:"]
             }
-            """;
+            """);
 
-    String json1 = """
+    Cfg json1 = new Cfg("""
             {
               "model": "text-davinci-003",
               "prompt": "answer 2+2 with only digit"
             }
-            """;
+            """);
 
-    String json2 = """
+    Cfg json2 = new Cfg("""
             {
               "model": "text-davinci-003",
               "prompt": "answer square root of 9 with only digits"
             }
-            """;
+            """);
 
-    String json3 = """
+    Cfg json3 = new Cfg("""
             {
               "model": "text-davinci-003",
               "prompt": "List of fruits which can be grown in US. Output only name of fruit, put each name on new line,max 2 fruits"
             }
-            """;
-    String json4 = """
+            """);
+    Cfg json4 = new Cfg("""
             {
               "model": "text-davinci-003",
               "prompt": "List 2 fruits which can be grown in US and also include Orange in the list. Output only name of fruit, put each name on new line"
             }
-            """;
+            """);
+    Cfg json5 = new Cfg(
+            "https://api.openai.com/v1/chat/completions",
+        """
+                {
+                  "model": "gpt-3.5-turbo",
+                  "messages": [{"role": "system", "content": "Follow instructions as is without additions."},
+                  {"role": "user", "content": "List 2 fruits which can be grown in US and also include Orange in the list. Output only name of fruit, put each name on new line"}],
+                  "temperature": 0.9,
+                  "max_tokens": 150,
+                  "top_p": 1,
+                  "frequency_penalty": 0,
+                  "presence_penalty": 0.6,
+                  "stop": [" Human:", " AI:"]
+                }
+                """);
+//    {
+//        "model": "text-davinci-003",
+//            "prompt": "List 2 fruits which can be grown in US and also include Orange in the list. Output only name of fruit, put each name on new line"
+//    }
     @Test
     public void test_query() throws URISyntaxException, IOException {
         String key = System.getenv("OPENAI_API_KEY");
 
-        final URI uri = new URIBuilder("https://api.openai.com/v1/completions")
+        Cfg cfg = json5;
+
+        final String url = cfg.url==null ? "https://api.openai.com/v1/completions" : cfg.url;
+        final URI uri = new URIBuilder(url)
                 .setCharset(StandardCharsets.UTF_8)
                 .build();
         final Request request = Request.post(uri).connectTimeout(Timeout.ofSeconds(5));//.socketTimeout(20000);
 
-        request.body(new StringEntity(json4, StandardCharsets.UTF_8));
+        request.body(new StringEntity(cfg.prompt, StandardCharsets.UTF_8));
 
         RestUtils.addHeaders(request);
         request.addHeader("Content-Type", "application/json");
