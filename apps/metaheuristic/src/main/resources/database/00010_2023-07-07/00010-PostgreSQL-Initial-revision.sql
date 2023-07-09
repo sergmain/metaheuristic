a2cc965c5b1a5956bd2d271ef20321527abacdc6
sergmain <serg_main@yahoo.com>
2023-07-08 20:19:25 -0700

    .

--- apps/metaheuristic/src/main/resources/database/00010_2023-07-07/00010-PostgreSQL-Initial-revision.sql
+++ apps/metaheuristic/src/main/resources/database/00010_2023-07-07/00010-PostgreSQL-Initial-revision.sql
@@ -1,4 +1,4 @@
---  Metaheuristic, Copyright (C) 2017-2021, Innovation platforms, LLC
+--  Metaheuristic, Copyright (C) 2017-2023, Innovation platforms, LLC
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -11,6 +11,7 @@
 --
 --  You should have received a copy of the GNU General Public License
 --  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 -- !!!!! DO NOT CHANGE THE TYPE OF ID FIELD. IT MUST BE NUMERIC(10, 0) !!!!!!!
 create table MH_IDS
