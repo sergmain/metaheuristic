d6e5fc356573a06b7d7431ed3e2319b00321a7d9
sergmain <serg_main@yahoo.com>
2024-01-30 15:50:47 -0800

    updated copyright with 2024 year

--- apps/commons/src/main/java/ai/metaheuristic/ww2003/document/tags/ww2003/package-info.java
+++ apps/commons/src/main/java/ai/metaheuristic/ww2003/document/tags/ww2003/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Metaheuristic, Copyright (C) 2017-2023, Innovation platforms, LLC
+ * Metaheuristic, Copyright (C) 2017-2024, Innovation platforms, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
