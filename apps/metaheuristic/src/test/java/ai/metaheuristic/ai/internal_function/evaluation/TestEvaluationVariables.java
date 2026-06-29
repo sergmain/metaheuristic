0873876022addc5c94c6a004d2cb43337b09961b
sergmain <serg_main@yahoo.com>
2026-06-29 11:24:22 -0700

    made sourceCodes' uids unique

--- apps/metaheuristic/src/test/java/ai/metaheuristic/ai/internal_function/evaluation/TestEvaluationVariables.java
+++ apps/metaheuristic/src/test/java/ai/metaheuristic/ai/internal_function/evaluation/TestEvaluationVariables.java
@@ -15,29 +15,17 @@
  */
 
 package ai.metaheuristic.ai.internal_function.evaluation;
-import ai.metaheuristic.api.EnumsApi;
 
 import ai.metaheuristic.ai.MhComplexTestConfig;
 import ai.metaheuristic.ai.dispatcher.beans.TaskImpl;
-import ai.metaheuristic.ai.spi.MhSpi;
-import ch.qos.logback.classic.LoggerContext;
+import ai.metaheuristic.api.EnumsApi;
 import lombok.SneakyThrows;
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.cache.test.autoconfigure.AutoConfigureCache;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
